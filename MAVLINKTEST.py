2fc8d0419fd22f0ae0e93a749853ee23751008a1
lbbkd <143010922+lbbkd@users.noreply.github.com>
2024-02-28 14:29:42 -0600

    Create MAVLINKTEST.py

---
+++ MAVLINKTEST.py
@@ -0,0 +1,9 @@
+from pymavlink import mavutil
+
+# Initialize a connection listening on a UDP port
+
+master = mavutil.mavlink_connection('127.0.0.1:14550')
+
+# Wait for a heartbeat, setiing the system component ID of remote system for the link
+master.wait_heartbeat()
+print("Heartbeat from system (system %u component %u)" % (master.target_system, master.target_component)
